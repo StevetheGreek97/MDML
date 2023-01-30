32ddb505af4b8c0d7ccc6d1bec2f259a720c7114
StevetheGreek97 <mavrianosstelios@icloud.com>
2023-01-30 17:22:08 +0100

    update conf keys

--- Preprocces.py
+++ Preprocces.py
@@ -1,16 +1,37 @@
 from Createfolders import CreateFolders as cf
 from Yamlhandler import YamlHandler as yamlh
-from TrajectoryTools import LoadTrajectories, ParseTrajectories
+from TrajectoryTools import LoadTrajectories, ParseTrajectory, down_sample
+from ImageTrasformation import xyz2rgb
+import tensorflow as tf
+import numpy as np
 
 
 def preprocess():
-    # cf('output')
-    path = yamlh('config.yml').read_key('masterpath')
+    confkeys = yamlh('config.yml') 
+    output_folder = f"{confkeys.read_key('savepath')}/output"
+    cf(output_folder).create_subfolders()
+     
+    path = confkeys.read_key('masterpath')
+
+    print('Loading trajecrories...')
     data  = LoadTrajectories().traj_from_dir(path + 'data/')
 
     for (name,  trajectory) in data.items():
-        traj = ParseTrajectories(name, trajectory).allign()
-        print(traj)
+        traj_obj = ParseTrajectory(name, trajectory)
+        print(traj_obj)
+        downsampled =  down_sample(traj_obj.allign(), confkeys.read_key('downsample_to'))
+
+        confkeys.update_yaml('height', downsampled[0].shape[0])
+        confkeys.update_yaml('lenght', downsampled[0].shape[1])
+
+        downsample_rgb = xyz2rgb(downsampled)
+        cf(f'{output_folder}/imgs/{name}').create()
+
+        for i in range(downsample_rgb.shape[0]):
+            tf.keras.utils.save_img(f'{output_folder}/imgs/{name}/{name}_{i}.jpg',downsample_rgb[i])
+        
+
+        
 
 
 
