c188bbbe215a6bd559898b88b19a6777b6ede66f
StevetheGreek97 <mavrianosstelios@icloud.com>
2023-01-31 15:58:49 +0100

    main module: controls all 3 parts

---
+++ main.py
@@ -0,0 +1,11 @@
+from Preprocces import preprocess
+from MachineLearning import ML
+from Mapping import mapping
+
+def main():
+    preprocess()
+    ML()
+    mapping()
+
+if __name__ == "__main__":
+    main()
