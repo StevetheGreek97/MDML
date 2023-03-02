0e6a4d1eed65357ac297e9c7abf664cc009e9d80
StevetheGreek97 <90909747+StevetheGreek97@users.noreply.github.com>
2023-03-02 02:56:41 +0100

    delete dead files

--- test.py
+++
@@ -1,9 +0,0 @@
-from Utils import write_to_file
-# from Vision.cam import GradCam
-# from Statistics import rmsf, RMSD
-def main():
-    print(write_to_file )
-
-if __name__ == '__main__':
-    main()
-
